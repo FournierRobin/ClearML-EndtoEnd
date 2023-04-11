f7734da38e046f7e8feccdd6f9668f4c62b31971
robinfournier <robin.fournier@regis-dgac.net>
2023-04-11 15:29:30 +0200

    experiments w/ logging hp / scalars and save model

--- housereg_experiment.py
+++ housereg_experiment.py
@@ -3,6 +3,10 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
+try:
+    import joblib
+except ImportError:
+    from sklearn.externals import joblib
 
 from clearml import Task
 task = Task.init(project_name='House Price Regression', task_name='housereg_experiment')
@@ -15,13 +19,14 @@ y = df_train['SalePrice']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-params = {'n_estimators': 50, 'learning_rate': 0.01}
+params = {'n_estimators': 100, 'learning_rate': 0.09}
 task.connect(params)
 
 gb = GradientBoostingRegressor(learning_rate=params['learning_rate'], n_estimators=params['n_estimators'])
 gb.fit(X_train, y_train)
 
 task.upload_artifact('GBR model',gb)
+joblib.dump(gb, 'gb.pkl', compress=True)
 
 y_pred_gb = gb.predict(X_test)
 
